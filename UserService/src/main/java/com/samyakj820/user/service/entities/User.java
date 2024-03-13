ff205acf59b9177876395f713c5a9201d0b954e4
Samyak Jain <samyakj820@gmail.com>
2024-03-13 22:28:12 +0530

    Created User Entity

---
+++ UserService/src/main/java/com/samyakj820/user/service/entities/User.java
@@ -0,0 +1,31 @@
+package com.samyakj820.user.service.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "ID")
+    private String userId;
+
+    @Column(name = "NAME", length = 20)
+    private String name;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "ABOUT")
+    private String about;
+
+}
