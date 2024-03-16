66aa70afdc3e04668b1d4d3693832abfff0988c2
Samyak Jain <samyakj820@gmail.com>
2024-03-16 18:35:18 +0530

    Added Rating Controller to create ratings and fetch using userId or hotelId

--- RatingService/src/main/java/com/samyakj820/rating/service/services/RatingServiceImpl.java
+++ RatingService/src/main/java/com/samyakj820/rating/service/services/RatingServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class RatingServiceImpl implements RatingService{
@@ -15,6 +16,8 @@ public class RatingServiceImpl implements RatingService{
 
     @Override
     public Rating create(Rating rating) {
+        String ratingId = UUID.randomUUID().toString();
+        rating.setRatingId(ratingId);
         return ratingRepository.save(rating);
     }
 
