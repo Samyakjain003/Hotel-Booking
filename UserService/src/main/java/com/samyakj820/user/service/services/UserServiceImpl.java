37d355d29c76ec43d087c1223698a217555148b1
Samyak Jain <samyakj820@gmail.com>
2024-03-17 16:05:15 +0530

    Added method to call Rating and Hotel service APIs to fetch rating and hotel info

--- UserService/src/main/java/com/samyakj820/user/service/services/UserServiceImpl.java
+++ UserService/src/main/java/com/samyakj820/user/service/services/UserServiceImpl.java
@@ -1,19 +1,30 @@
 package com.samyakj820.user.service.services;
 
+import com.samyakj820.user.service.entities.Hotel;
+import com.samyakj820.user.service.entities.Rating;
 import com.samyakj820.user.service.entities.User;
 import com.samyakj820.user.service.exceptions.ResourceNotFoundException;
 import com.samyakj820.user.service.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
-
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public User saveUser(User user) {
@@ -24,12 +35,18 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getAllUser() {
-        return userRepository.findAll();
+        List<User> users = userRepository.findAll();
+        for(User user : users) {
+            fetchUserRatings(user);
+        }
+        return users;
     }
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id " + userId + " is not found on server!!"));
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id " + userId + " is not found on server!!"));
+        fetchUserRatings(user);
+        return user;
     }
 
     @Override
@@ -40,5 +57,19 @@ public class UserServiceImpl implements UserService {
     @Override
     public User updateUser(User user) {
         return userRepository.save(user);
+    }
+
+    public void fetchUserRatings(User user) {
+        Rating ratings[] = restTemplate.getForObject("http://localhost:8083/ratings/users/" + user.getUserId(), Rating[].class);
+        for(Rating rating : ratings) {
+            fetchHotelForRating(rating);
+        }
+        user.setRatings(Arrays.asList(ratings));
+    }
+
+    public void fetchHotelForRating(Rating rating) {
+        ResponseEntity<Hotel> response = restTemplate.getForEntity("http://localhost:8082/hotels/" + rating.getHotelId(), Hotel.class);
+        Hotel hotel = response.getBody();
+        rating.setHotel(hotel);
     }
 }
