6094e41ddfedb757bff6f6f1f253607ef0d42db6
Samyak Jain <samyakj820@gmail.com>
2024-03-18 22:46:53 +0530

    Added Config sever to the project

---
+++ ConfigServer/src/test/java/com/samyakj820/config/server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samyakj820.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
