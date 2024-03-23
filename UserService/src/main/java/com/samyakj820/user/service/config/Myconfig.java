d097acf94dbb5bdf993ff93332d9fbc83efbf5e7
Samyak Jain <samyakj820@gmail.com>
2024-03-23 23:53:35 +0530

    Created RestTemplateClientInterceptor

--- UserService/src/main/java/com/samyakj820/user/service/config/Myconfig.java
+++ UserService/src/main/java/com/samyakj820/user/service/config/Myconfig.java
@@ -1,8 +1,11 @@
 package com.samyakj820.user.service.config;
 
+import com.samyakj820.user.service.config.interceptor.RestTemplateInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
@@ -11,12 +14,26 @@ import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedCli
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 public class Myconfig {
+
+    @Autowired
+    private  ClientRegistrationRepository clientRegistrationRepository;
+    @Autowired
+    private OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository;
 
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
+
+        RestTemplate restTemplate = new RestTemplate();
+        List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add(new RestTemplateInterceptor(manager(clientRegistrationRepository, oAuth2AuthorizedClientRepository)));
+
+        restTemplate.setInterceptors(interceptors);
         return new RestTemplate();
     }
 
