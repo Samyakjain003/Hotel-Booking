745cdd04c4a2e866c03bfd15f7a4f27acb68b876
Samyak Jain <samyakj820@gmail.com>
2024-03-24 00:13:57 +0530

    Added Authorization on Mappings based on SCOPE

--- HotelService/src/main/java/com/samyakj820/hotel/service/controllers/HotelController.java
+++ HotelService/src/main/java/com/samyakj820/hotel/service/controllers/HotelController.java
@@ -5,6 +5,7 @@ import com.samyakj820.hotel.service.services.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,26 +18,31 @@ public class HotelController {
     @Autowired
     private HotelService hotelService;
 
+    @PreAuthorize("hasAuthority('Admin')")
     @PostMapping
     public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel) {
         return ResponseEntity.status(HttpStatus.CREATED).body(hotelService.saveHotel(hotel));
     }
 
+    @PreAuthorize("hasAuthority('SCOPE_internal')")
     @GetMapping("/{hotelId}")
     public ResponseEntity<Hotel> getHotel(@PathVariable String hotelId) {
         return ResponseEntity.status(HttpStatus.OK).body(hotelService.getHotel(hotelId));
     }
 
+    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")
     @GetMapping
     public ResponseEntity<List<Hotel>> getHotels() {
         return ResponseEntity.of(Optional.ofNullable(hotelService.getAllHotels()));
     }
 
+    @PreAuthorize("hasAuthority('Admin')")
     @PutMapping
     public ResponseEntity<Hotel> updateHotel(@RequestBody Hotel hotel) {
         return ResponseEntity.status(HttpStatus.OK).body(hotelService.updateHotel(hotel));
     }
 
+    @PreAuthorize("hasAuthority('Admin')")
     @DeleteMapping("/{hotelId}")
     public void deleteHotel(@PathVariable String hotelId) {
         hotelService.deleteHotel(hotelId);
--- RatingService/src/main/java/com/samyakj820/rating/service/controllers/RatingController.java
+++ RatingService/src/main/java/com/samyakj820/rating/service/controllers/RatingController.java
@@ -5,6 +5,7 @@ import com.samyakj820.rating.service.services.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,21 +17,25 @@ public class RatingController {
     @Autowired
     private RatingService ratingService;
 
+    @PreAuthorize("hasAuthority('Admin')")
     @PostMapping
     public ResponseEntity<Rating> create(@RequestBody Rating rating) {
         return ResponseEntity.status(HttpStatus.CREATED).body(ratingService.create(rating));
     }
 
+    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")
     @GetMapping
     public ResponseEntity<List<Rating>> getAllRatings() {
         return ResponseEntity.ok(ratingService.getAllRatings());
     }
 
+    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")
     @GetMapping("/users/{userId}")
     public ResponseEntity<List<Rating>> getAllRatingsByUserId(@PathVariable String userId) {
         return ResponseEntity.ok(ratingService.getAllRatingsByUserId(userId));
     }
 
+    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")
     @GetMapping("/hotels/{hotelId}")
     public ResponseEntity<List<Rating>> getAllRatingsByHotelId(@PathVariable String hotelId) {
         return ResponseEntity.ok(ratingService.getAllRatingsByHotelId(hotelId));
